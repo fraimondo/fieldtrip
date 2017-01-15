945088de2724a1d9128fa89769cdcefa1215ddb8
Fede Raimondo <federaimondo@gmail.com>
2017-01-15 19:46:15 +0100

    OSX 10.12

--- realtime/src/buffer/src/clock_gettime.c
+++ realtime/src/buffer/src/clock_gettime.c
@@ -15,16 +15,16 @@
 /* OS X does not have clock_gettime, make a drop-in replacement that uses clock_get_time */
 /* the first argument to this function is CLOCK_REALTIME, which gets ignored */
 
-int clock_gettime(int ignore, struct timespec *ts) {
-  clock_serv_t cclock;
-  mach_timespec_t mts;
-  host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
-  clock_get_time(cclock, &mts);
-  mach_port_deallocate(mach_task_self(), cclock);
-  ts->tv_sec  = mts.tv_sec;
-  ts->tv_nsec = mts.tv_nsec;
-  return 0;
-}
+// int clock_gettime(int ignore, struct timespec *ts) {
+//   clock_serv_t cclock;
+//   mach_timespec_t mts;
+//   host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+//   clock_get_time(cclock, &mts);
+//   mach_port_deallocate(mach_task_self(), cclock);
+//   ts->tv_sec  = mts.tv_sec;
+//   ts->tv_nsec = mts.tv_nsec;
+//   return 0;
+// }
 
 #elif defined (COMPILER_MINGW)
 
