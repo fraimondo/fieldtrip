ab7860fc7fec9573dbee53b5680cec75b0280611
Fede Raimondo <federaimondo@gmail.com>
2017-01-15 19:46:25 +0100

    OSX 10.12

--- realtime/src/buffer/src/osx/clock_gettime.h
+++ realtime/src/buffer/src/osx/clock_gettime.h
@@ -2,7 +2,6 @@
 
 /* OS X does not have clock_gettime, make a drop-in replacement that uses clock_get_time */
 /* the first argument to this function is CLOCK_REALTIME, which gets ignored */
-int clock_gettime(int ignore, struct timespec *tp);
+// int clock_gettime(int ignore, struct timespec *tp);
 
-#define CLOCK_REALTIME 0
-
+// #define CLOCK_REALTIME 0
